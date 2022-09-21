2067ee11578d18b2cc1e9b75d5f6d37fcc2327c2
unknown <caiomeloml@gmail.com>
2022-09-21 20:57:19 -0300

    atualizando dados

---
+++ Oficina/inserindo_dados_Oficina.sql
@@ -0,0 +1,7 @@
+show databases;
+use oficina;
+desc cadastrodepessoas;
+select * from mecanicos;
+select * from cadastrodepessoas;
+select * from cliente_veiculo;
+INSERT INTO cadastrodepessoas VALUES ('02','Fulano', 'Rua do oeste', '11111111112', '91-991919191 fulano@hotmail.com')
